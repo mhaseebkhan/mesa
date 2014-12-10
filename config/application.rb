45dc19bbc5aa055df5c65abd6a816e3fa6c9214f
amir <amir.yamin@tkxel.com>
2014-12-10 14:46:13 +0000

    setting variables for production deployment on heroku

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Mesa
   class Application < Rails::Application
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = true
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
